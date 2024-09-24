d5e1cc4e61ffcf090d01d8254fc227de84e469a9
Truong Thien <truongthien144@gmail.com>
2024-09-24 21:35:09 +0700

    finish ex1

---
+++ Core/Inc/ex1.h
@@ -0,0 +1,15 @@
+/*
+ * ex1.h
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: TruongThien
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+
+#endif /* INC_EX1_H_ */
---
+++ Core/Src/ex1.c
@@ -0,0 +1,38 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: TruongThien
+ */
+#include "ex1.h"
+
+void  display7SEG(int num){
+	const uint8_t segmentPatterns[10] = {
+	        0b11000000, // 0
+	        0b11111001, // 1
+	        0b10100100, // 2
+	        0b10110000, // 3
+	        0b10011001, // 4
+	        0b10010010, // 5
+	        0b10000010, // 6
+	        0b11111000, // 7
+	        0b10000000, // 8
+	        0b10010000  // 9
+	};
+
+	if (num < 0 || num > 9){
+		return;// Handle invalid input
+	}
+
+	uint8_t segments = segmentPatterns[num];
+
+	// Set the GPIO pins according to the 7-segment pattern for the digit
+	HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, (segments & 0x01) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, (segments & 0x02) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, (segments & 0x04) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, (segments & 0x08) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, (segments & 0x10) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, (segments & 0x20) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, (segments & 0x40) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
