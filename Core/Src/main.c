ac261d3e9d782f32e7fab055d5a242cf897febe9
Truong Thien <truongthien144@gmail.com>
2024-09-25 10:47:41 +0700

    update ex9

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -60,7 +60,7 @@ int timer0_counter = 0;
 int timer0_flag = 0;
 int timer1_counter = 0;
 int timer1_flag = 0;
-int TIMER_CYCLE = 1;
+int TIMER_CYCLE = 10;
 
 void setTimer0(int duration){
     timer0_counter = duration / TIMER_CYCLE;
